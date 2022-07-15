f143a914cd86e6f2b088c09e0a0e4b99056b39cc
ru5s <hellperock@gmail.com>
2022-07-15 10:53:05 +0500

    Initial Commit

---
+++ secondGestureRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  secondGestureRecognizer
+//
+//  Created by Ruslan Ismailov on 15/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
